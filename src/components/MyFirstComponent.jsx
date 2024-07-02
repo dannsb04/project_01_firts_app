d65b5c256044a59ad1f28aedcca0ef8995cdfc9d
dannsb04 <danielsbmo@gmail.com>
2024-07-01 19:49:38 -0500

    my first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Myfirstcomponent = () => {
+  return (
+    <>
+    <div>Myfirstcomponent</div>
+    <div>Hello world, Im Daniel Muñoz</div>
+    <div>27 de junio de 2024 </div>
+    </>
+    
+  )
+}
